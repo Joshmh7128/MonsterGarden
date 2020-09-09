06471b39520f9b71e3f372c42c30554d76cd507e
Clare <clareohara0@gmail.com>
2020-09-09 13:11:36 -0400

    Added to Action Script/Dantler AI

--- Assets/Scripts/Monster AI/ActionScript.cs
+++ Assets/Scripts/Monster AI/ActionScript.cs
@@ -7,26 +7,37 @@ using JM.LinqFaster;
 
 public class ActionScript : MonoBehaviour
 {
+    // ** Scripts
     [SerializeField] protected ObjectTrackingClass objectTrackingClass; // our object tracking class
-    [SerializeField] protected Text currentStatus; // what is the current status of the deer?
-    [SerializeField] protected float maxExploreBoundsX; // max X explore?
-    [SerializeField] protected float maxExploreBoundsZ; // max Z explore?
-    [SerializeField] protected float startY; // our Y for logging
+
+    // ** Monster Info
+    [SerializeField] public Text currentStatus; // what is the current status of the deer?
+    protected bool wasDisabled = false; // check to see if animal was disabled then enabled
     [SerializeField] protected float movementSpeed; // how fast can we move?
-    [SerializeField] protected Vector3 targetPosition; // where are we headed?
     [SerializeField] protected bool behaviourActive; // is our behaviour active?
+    [SerializeField] protected float widthOfAnimal; // width of animal // todo: make sure works well with rotating
+    [SerializeField] protected GameObject home; // claimed home of animal
+    [SerializeField] protected List<GameObject> friendList = new List<GameObject>(3);
     [SerializeField] protected string restingSpotType; // example: "Soft-Dry"
     [SerializeField] protected string feedingSpotType; // example: "Grass"
-    [SerializeField] protected List<GameObject> restingSpotList;
-    [SerializeField] protected List<FeedingSpotClass> feedingSpotList;
+    protected List<GameObject> restingSpotList; // list of resting spots pulling from
+    protected List<FeedingSpotClass> feedingSpotList; // list of feeding spots pulling from
 
-    // start
+    // ** Movement/Herding Stats
+    [SerializeField] protected float maxExploreBoundsX; // max X explore?
+    [SerializeField] protected float maxExploreBoundsZ; // max Z explore?
+    [SerializeField] protected float startY; // our Y for logging // todo: change around what relys on this once heights can change
+    [SerializeField] protected Vector3 targetPosition; // where are we headed?
+    protected float minDistBetweenHerdAnimals; // based on width of animal to avoid overlapping
+    
+
     public virtual void Start()
     {
         // set our Y
         startY = transform.position.y;
         // set our object tracking class
         objectTrackingClass = WorldManager.objectTrackingClass;
+        minDistBetweenHerdAnimals = widthOfAnimal/Mathf.Sqrt(2);
         if (restingSpotType == "Soft-Dry") restingSpotList = objectTrackingClass.softDryRestingSpotList; // todo: make good
         if (feedingSpotType == "Grass") feedingSpotList = objectTrackingClass.grassFeedingSpotList;
     }
@@ -37,8 +48,9 @@ public class ActionScript : MonoBehaviour
         // go to random location, explore!
         targetPosition = new Vector3(Random.Range(-maxExploreBoundsX, maxExploreBoundsX), startY, Random.Range(-maxExploreBoundsZ, maxExploreBoundsZ));
         currentStatus.text = "Exploring";
-        yield return StartCoroutine(MoveTo());
+        yield return MoveTo(this.gameObject, targetPosition);
         yield return new WaitForSeconds(Random.Range(0, 8)); // 14
+        // Debug.Log(gameObject.name + " done Exploring");
     }
 
     // required actions: eat at 3 grass spots, rest at one resting spot
@@ -46,11 +58,12 @@ public class ActionScript : MonoBehaviour
     protected IEnumerator Rest()
     {
         // find a place to rest
-        currentStatus.text = "Finding Resting Spot";
+        // Debug.Log(gameObject.name + " starting to rest");
         List<GameObject> tempList = restingSpotList.WhereF(x => !objectTrackingClass.objectsinUseTracking.Contains(x));
         if (tempList.Count > 0)
         {
             // get resting spot
+            currentStatus.text = "Finding Resting Spot";
             GameObject targetRestSpot = tempList[Random.Range(0, tempList.Count)];
             if (targetRestSpot == null)
             {
@@ -62,7 +75,7 @@ public class ActionScript : MonoBehaviour
             // get position of resting spot
             targetPosition = new Vector3(targetRestSpot.transform.position.x, transform.position.y, targetRestSpot.transform.position.z);
             // move to resting spot
-            yield return MoveTo();
+            yield return MoveTo(this.gameObject, targetPosition);
             // rest 3 - 5 times, checking if the resting spot is there in between
             currentStatus.text = "Resting";
             for (int i = 0; i< Random.Range(3,6); i++)
@@ -76,17 +89,21 @@ public class ActionScript : MonoBehaviour
             // unmark as in use
             if (targetRestSpot != null) objectTrackingClass.objectsinUseTracking.Remove(targetRestSpot);
         }
-        else Debug.Log("All resting spots in use");
+        else
+        {
+            Debug.Log("All resting spots in use");
+        }
     }
 
     protected IEnumerator Eat()
     {
         // we're finding food
-        currentStatus.text = "Finding Food";
-        List<FeedingSpotClass> tempList = feedingSpotList.WhereF(x => !objectTrackingClass.objectsinUseTracking.Contains(x.gameObject));
+        // Debug.Log(gameObject.name + " starting to eat");
+        List<FeedingSpotClass> tempList = feedingSpotList.WhereF(x => !objectTrackingClass.objectsinUseTracking.Contains(x.gameObject) && x.hasFood);
         if (tempList.Count > 0)
         {
             // get food spot
+            currentStatus.text = "Finding Food";
             GameObject targetFood = tempList[Random.Range(0, tempList.Count)].gameObject;
             if (targetFood == null)
             {
@@ -98,7 +115,7 @@ public class ActionScript : MonoBehaviour
             //Debug.Log(targetFood.name + " is in use by " + this.gameObject.name);
             // get food spot position and move to it
             targetPosition = new Vector3(targetFood.transform.position.x, transform.position.y, targetFood.transform.position.z);
-            yield return StartCoroutine(MoveTo());
+            yield return MoveTo(this.gameObject, targetPosition);
             // if food is still there when you get there, eat
             if (targetFood != null)
             {
@@ -109,20 +126,175 @@ public class ActionScript : MonoBehaviour
             }
             else // if the food is gone start exploring
             {
-                yield return StartCoroutine(Explore());
+                yield return Explore();
             }
         }
-        else Debug.Log("All feeding spots in use");
-    }
-
-    protected IEnumerator MoveTo()
-    {
-        while (Vector3.Distance(transform.position, targetPosition) > 0.000001) // go until in the middle of hex
+        else
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementSpeed * Time.deltaTime);
-            yield return 0;
+            Debug.Log("All feeding spots in use");
         }
     }
+
+    protected IEnumerator MoveTo(GameObject gameObject, Vector3 targetPosition)
+    {
+        while (Vector3.Distance(gameObject.transform.position, targetPosition) > 0.000001)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPosition, movementSpeed * Time.deltaTime);
+            yield return 0;
+        }
+        yield return true;
+    }
+
+    // Todo: Zone of Clare needs to set up the requirements
+    // ** If certain requirements are met; get what they are
+    protected void ClaimHome(GameObject claimedHome)
+    {
+        objectTrackingClass.objectsinUseTracking.Add(claimedHome);
+        home = claimedHome;
+    }
+
+    protected void LeaveGarden()
+    {
+
+    }
+
+    protected void MakeFriend(GameObject newFriend)
+    {
+        if (!friendList.Contains(newFriend)) friendList.Add(newFriend);
+    }
+
+    protected void RemoveFriend(GameObject friend)
+    {
+        friendList.Remove(friend);
+    }
+
+    protected IEnumerator Herding(List<GameObject> herdingFollowers)
+    {
+        if (herdingFollowers.Count != 0)
+        {    
+            // List of coroutines currently taking place within this method, used to make sure the monsters wait for the other monsters to reach certain positions
+            List<CoroutineWithData> waitList = new List<CoroutineWithData>();
+            // List of 4 positions in a square around the lead monster
+            List<Vector3> posList = new List<Vector3>()
+            {
+                new Vector3(transform.position.x + minDistBetweenHerdAnimals, transform.position.y, transform.position.z + minDistBetweenHerdAnimals),
+                new Vector3(transform.position.x - minDistBetweenHerdAnimals, transform.position.y, transform.position.z + minDistBetweenHerdAnimals),
+                new Vector3(transform.position.x + minDistBetweenHerdAnimals, transform.position.y, transform.position.z - minDistBetweenHerdAnimals),
+                new Vector3(transform.position.x - minDistBetweenHerdAnimals, transform.position.y, transform.position.z - minDistBetweenHerdAnimals)
+            };
+            currentStatus.text = "Leading Herd";
+            foreach (GameObject follower in herdingFollowers)
+            {
+                // get the closest spot in the position list
+                posList.OrderByF(x => Vector3.Distance(x, follower.transform.position));
+                // choose a random position within a sphere (big enough for no overlap) around the chosen spot 
+                Vector3 lerpCirclePosition = posList[0] + (Random.insideUnitSphere*widthOfAnimal/2);
+                // since its a random spot in a sphere, need to fix y
+                lerpCirclePosition.y = transform.position.y;
+                // add coroutine to waitlist so the monsters wait until they are in herd position
+                waitList.Add(new CoroutineWithData(this, MoveTo(follower, lerpCirclePosition)));
+                posList.RemoveAt(0);
+            }
+            foreach(CoroutineWithData c in waitList) // wait until all monsters are in herd position // todo: see if the whole coroutine with data can be improved
+            {
+                yield return new WaitUntil(() => c.done);
+            }
+            waitList.Clear();
+            // get random spot to travel as a pack to (explore together)
+            targetPosition = new Vector3(Random.Range(-maxExploreBoundsX, maxExploreBoundsX), startY, Random.Range(-maxExploreBoundsZ, maxExploreBoundsZ));
+            yield return new WaitForSeconds(1);
+            // make monsters wait until they get to the randomly chosen spot ---
+            foreach(GameObject follower in herdingFollowers)
+            {
+                waitList.Add(new CoroutineWithData(this, MoveTo(follower, follower.transform.position + targetPosition - transform.position)));
+            }
+            waitList.Add(new CoroutineWithData(this, MoveTo(this.gameObject, targetPosition)));
+            foreach(CoroutineWithData c in waitList)
+            {
+                yield return new WaitUntil(() => c.done);
+            }
+            yield return new WaitForSeconds(2); // ** edit timing as needed
+            // -------------------------------------------------------------------
+            foreach(GameObject friend in herdingFollowers)
+            {
+                // Restart Choose Behavior by disabling/enabling
+                friend.SetActive(false);
+                friend.SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.Log("No followers available");
+        }
+    }
+
+    protected IEnumerator GeneralTurfDispute(GameObject enemy) // TODO: do it
+    {
+        yield return 0;
+        // Do actions then...
+        // Restart Choose Behavior by disabling/enabling
+        enemy.SetActive(false);
+        enemy.SetActive(true);
+    }
+
+
+    public class CoroutineWithData // https://answers.unity.com/questions/24640/how-do-i-return-a-value-from-a-coroutine.html
+    {
+        public Coroutine coroutine { get; private set; }
+        public IEnumerator target;
+        public bool done;
+
+        public CoroutineWithData(MonoBehaviour owner, IEnumerator target)
+        {
+            this.target = target;
+            this.coroutine = owner.StartCoroutine(Run());
+        }
+
+        private IEnumerator Run()
+        {
+            done = false;
+            while(target.MoveNext())
+            {
+                yield return target.Current;
+            }
+        done = true;
+        }
+    }
+
+    public void StopCurrentCoroutines()
+    {
+        StopAllCoroutines();
+    }
+
+    void OnDisable()
+    {
+        if (!wasDisabled) wasDisabled = true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /*protected IEnumerator FlyAround() // has a little bounce to it, want to start going a random direction and then move within angles
     {
---
+++ Assets/Scripts/Monster AI/Grassland Monsters/DantlerAI.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JM.LinqFaster;
+
+public class DantlerAI : ActionScript
+{
+    // statuses of actions that shouldn't be interrupted by other actions // ** changed around as needed
+    private List<string> uninterruptableActions = new List<string>{"Eating", "Resting", "Finding Resting Spot", "Finding Food", "Herding", "Leading Herd"};
+    
+    public override void Start()
+    {
+        base.Start();
+        StartCoroutine(ChooseBehaviour());
+    }
+
+    void OnEnable()
+    {
+        // if monster was disabled, need to restart ChooseBehavior
+        if (wasDisabled) 
+        {
+            StartCoroutine(ChooseBehaviour());
+        }
+    }
+
+    private IEnumerator ChooseBehaviour()
+    {
+        yield return Explore();
+        while(true)
+        {
+            int i = Random.Range(0, 4);
+            currentStatus.text = null;
+            switch (i)
+            {
+                case 0:
+                    yield return Explore();
+                    break;
+
+                case 1:
+                    yield return Rest();
+                    break;
+
+                case 2:
+                    yield return Eat();
+                    break;
+                case 3:
+                    yield return new WaitForEndOfFrame();
+                    Debug.Log(this.gameObject.name);
+                    List<GameObject> followerList = new List<GameObject>(); // monsters following this monster
+                    foreach(GameObject go in friendList) // ** change to be other monsters outside of friendlist maybe
+                    {
+                        ActionScript temp = go.GetComponent<ActionScript>();
+                        if (!uninterruptableActions.Contains(temp.currentStatus.text))
+                        {
+                            followerList.Add(go);
+                            temp.currentStatus.text = "Herding";
+                            temp.StopCurrentCoroutines();
+                        }
+                    }
+                    yield return Herding(followerList);
+                    break;
+                case 4:
+                    yield return new WaitForEndOfFrame();
+                    // todo: 
+                    List<GameObject> tempGameObjectList = friendList.WhereF(x => !uninterruptableActions.Contains(x.GetComponent<ActionScript>().currentStatus.text));
+                    GameObject enemyToFight = tempGameObjectList[Random.Range(0, tempGameObjectList.Count)];
+                    enemyToFight.GetComponent<ActionScript>().StopAllCoroutines();
+                    yield return GeneralTurfDispute(enemyToFight);
+                    break;
+            }
+        }
+    }
+}
